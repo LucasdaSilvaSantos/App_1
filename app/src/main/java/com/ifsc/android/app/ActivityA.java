3a78eaf486f797b999dea99cd84381e44f28bffe
Lucas da Silva Santos <ldsantos2004@gmail.com>
2026-08-24 22:20:15 -0300

    adicionando mais telas, nelas contendo imagens e intenções. Indo da atividade A para B e C

--- app/src/main/java/com/ifsc/android/app/ActivityA.java
+++ app/src/main/java/com/ifsc/android/app/ActivityA.java
@@ -1,19 +1,49 @@
 package com.ifsc.android.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
-public class CiclodeVida extends AppCompatActivity {
+public class ActivityA extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_a);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
         Log.d("CiclodeVida","oncreate");
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(view -> {
+           Intent i = new Intent(this, ActivityB.class);
+           startActivity(i);
+        });
+
+        Button b2 = findViewById(R.id.button2);
+        b2.setOnClickListener(view -> {
+            EditText editText=findViewById(R.id.edText);
+            String s = editText.getText().toString();
+
+            Intent i = new Intent(this, ActivityC.class);
+            i.putExtra("msg",s);
+            startActivity(i);
+        });
     }
 
     @Override
